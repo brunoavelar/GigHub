f109877b540fb4b8dbfa562e25674de752835890
Bruno Avelar <brunoavelar@gmail.com>
2016-06-04 21:05:14 -0400

    Add gig list and going button

---
+++ GigHub/Controllers/AttendencesController.cs
@@ -0,0 +1,45 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    
+
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            string userId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(x => x.GigId == dto.GigId && x.AttendeeId == userId))
+                return BadRequest("The attendance already exists.");
+
+            var attendence = new Attendance()
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+            };
+
+            _context.Attendances.Add(attendence);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
