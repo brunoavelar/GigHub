3aa20624a6470f875271a250a0bbac80dd7dcf30
Bruno Avelar <brunoavelar@gmail.com>
2016-06-10 13:09:40 -0400

    AttendanceController and Models tests

---
+++ GigHub.Tests/Controllers/Api/AttentanceControllerTests.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+using GigHub.Controllers.Api;
+using GigHub.Core;
+using GigHub.Core.Dtos;
+using GigHub.Core.Models;
+using GigHub.Core.Repositories;
+using GigHub.Tests.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Web.Http.Results;
+
+namespace GigHub.Tests.Controllers.Api
+{
+    [TestClass]
+    public class AttentanceControllerTests
+    {
+        private AttendancesController controller;
+        private Mock<IAttendanceRepository> repository;
+        private string userId;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            repository = new Mock<IAttendanceRepository>();
+
+            var mockUoW = new Mock<IUnitOfWork>();
+            mockUoW.SetupGet(x => x.Attendances).Returns(repository.Object);
+
+            userId = "1";
+
+            controller = new AttendancesController(mockUoW.Object);
+            controller.MockCurrentUser(userId, "user01@user.com");
+        }
+
+        [TestMethod]
+        public void Attend_UserAttendingAGigForWhichHeHasAnAttendance_ShouldReturnBadRequest()
+        {
+            int gigId = 1;
+            var attendance = new Attendance();
+
+            repository.Setup(r => r.GetAttendance(userId, gigId)).Returns(attendance);
+
+            var dto = new AttendanceDto
+            {
+                GigId = gigId
+            };
+            var result = controller.Attend(dto);
+            result.Should().BeOfType<BadRequestErrorMessageResult>();
+
+        }
+
+        [TestMethod]
+        public void Attend_WithValidRequest_ShouldReturnOk()
+        {
+            var dto = new AttendanceDto
+            {
+                GigId = 1
+            };
+            var result = controller.Attend(dto);
+            result.Should().BeOfType<OkResult>();
+        }
+
+        [TestMethod]
+        public void Delete_NoAttendanceWithGivenIdExists_ShouldReturnNotFound()
+        {
+            var result = controller.DeleteAttendance(1);
+            result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [TestMethod]
+        public void Delete_WithValidRequest_ShouldReturnOk()
+        {
+            int gigId = 1;
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = userId
+            };
+
+            repository.Setup(r => r.GetAttendance(userId, gigId)).Returns(attendance);
+
+            var result = controller.DeleteAttendance(gigId);
+            result.Should().BeOfType<OkResult>();
+        }
+    }
+}
---
+++ GigHub.Tests/Models/GigTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using GigHub.Core.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace GigHub.Tests.Models
+{
+    [TestClass]
+    public class GigTests
+    {
+        [TestMethod]
+        public void Cancel_WhenCalled_ShouldSetIsCanceledToTrue()
+        {
+            var gig = new Gig();
+
+            gig.Cancell();
+            gig.IsCanceled.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Cancel_WhenCalled_EachAttendeeShouldHaveANotification()
+        {
+            var gig = new Gig();
+            var user = new ApplicationUser { Id = "1" };
+            var attendance = new Attendance { Attendee = user };
+            gig.Attendances.Add(attendance);
+
+            gig.Cancell();
+
+            var attendees = gig.Attendances.Select(a => a.Attendee).ToList();
+            attendees.FirstOrDefault().UserNotifications.Count().Should().Be(1);
+        }
+    }
+}
